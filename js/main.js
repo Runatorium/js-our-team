fe0959264f8a62f73bee35000360191c12943de5
Runatorium <stefano.alesci1@gmail.com>
2023-03-08 15:29:00 +0100

    array con oggetti (membriteam) creato

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let MembriTeam = [
+    {
+        'nome' : 'Wayne Barnett',
+        'ruolo': 'Founder & CEO',
+        'foto url': 'wayne-barnett-founder-ceo.jpg' 
+    },
+    {
+        'nome' : 'Angela Caroll',
+        'ruolo': 'Chief Editor',
+        'foto url': 'angela-caroll-chief-editor.jpg' 
+    },
+    {
+        'nome' : 'Walter Gordon',
+        'ruolo': 'Office Manager',
+        'foto url': 'walter-gordon-office-manager.jpg' 
+    },
+    {
+        'nome' : 'Angela Lopez',
+        'ruolo': 'Social Media Manager',
+        'foto url': 'angela-lopez-social-media-manager.jpg' 
+    },
+    {
+        'nome' : 'Scott Estrada',
+        'ruolo': 'Developer',
+        'foto url': 'scott-estrada-developer.jpg' 
+    },
+    {
+        'nome' : 'Barbara Ramos',
+        'ruolo': 'Graphic Designer',
+        'foto url': 'barbara-ramos-graphic-designer.jpg' 
+    },
+]
